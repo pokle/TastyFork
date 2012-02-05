5a3deacd1c1cabf3bd217f5cde4782aae0c1eadd
Tushar Pokle <tushar.pokle@gmail.com>
2012-02-05 17:33:27 +1100

    objects are now versioned

--- test/foundation-tests.js
+++ test/foundation-tests.js
@@ -5,57 +5,75 @@ var tf = require('tastyfork');
 
 module.exports = {
 
-  'works like a map': function(beforeExit, assert) {
+  'merges a new object as version 0': function(beforeExit, assert) {
     var repo = tf.create();
+    var obj = repo.merge({id: 'x', foo: 'poo'});
     
-    repo.set('a', 1);
-    repo.set('b', 3);
-    repo.set('a', 2);
+    assert.eql({id: 'x', foo: 'poo', ver: 0}, obj);
+  },
+
+  'merges an existing object with one plus the version': function(beforeExit, assert) {
+    var repo = tf.create();
+    var obj = repo.merge({id: 'x', foo: 'poo'});
+
+    obj.ver = obj.ver + 1;
+    obj.foo = 'yow';
     
-    assert.equal(2, repo.get('a'));
-    assert.equal(3, repo.get('b'));
+    var updated = repo.merge(obj);
+    assert.eql({id: 'x', foo: 'yow', ver: 1}, updated);
+  },
+  
+  'rejects merge with incorrect version': function(beforeExit, assert) {
+    var repo = tf.create();
+    var obj = repo.merge({id: 'x', foo: 'poo'});
+
+    obj.ver = obj.ver + 100;
+    obj.foo = 'yow';
+    
+    var updated = repo.merge(obj);
+    assert.eql({id: 'x', foo: 'poo', ver: 0}, updated); // Unchanged
   },
 
   'pulling brings in changes from origin': function(beforeExit, assert) {
     var origin = tf.create();
-    origin.set('a', 1);
+    var obj = origin.merge({id: 'x', foo: 'poo'});
     
     var local = tf.create(origin);
-
-    assert.equal(undefined, local.get('a')); // Before
+  
+    assert.equal(undefined, local.get('x')); // Before
     local.pull();
-    assert.equal(1, local.get('a')); // After
+    assert.eql(obj, local.get('x')); // After
   },
-
+  
   "pulling doesn't destroy un-sync'd local changes": function(beforeExit, assert) {
-
+  
     var origin = tf.create();
-    origin.set('color', "pink");
+    origin.merge({id: 'x', color: "pink"});
     
     var local = tf.create(origin);
-    local.set('kind', 'mooo'); // Supposedly un-sync'd local change
-
+    local.merge({id: 'y', kind: 'mooo'}); // Supposedly un-sync'd local change
+  
     // Before
-    assert.equal("mooo", local.get('kind'));
-    assert.equal(undefined, local.get('color'));
+    assert.eql({id: 'y', kind: 'mooo', ver: 0}, local.get('y'));
+    assert.eql(undefined, local.get('x'));
     
     local.pull();
     
     // After
-    assert.equal("mooo", local.get('kind'));  // Unaffected
-    assert.equal("pink", local.get('color')); // Updated
+    assert.eql({id: 'y',  kind: 'mooo', ver: 0}, local.get('y'));  // Unaffected
+    assert.eql({id: 'x', color: 'pink', ver: 0}, local.get('x'));// Pulled
   },
+  
 
   "Local changes don't affect the origin": function(beforeExit, assert) {
     var origin = tf.create();
-    origin.set('a', 1);
+    origin.merge({id: 'x', color: "pink"});
     
     var local = tf.create(origin);
     local.pull();
-    local.set('a', 7);
+    local.merge({id: 'x', color: 'blue', ver: 1});
     
-    assert.equal(1, origin.get('a'));
-  },
-
-  
+    assert.eql({id: 'x', color: 'blue', ver: 1},  local.get('x')); // Updated
+    assert.eql({id: 'x', color: 'pink', ver: 0}, origin.get('x')); // Unaffected
+  }  
 }
